fe26e04683a489aa8306de0579dfddd72ba81c83
JoeTags <clausoldenburg458@gmail.com>
2022-01-26 12:17:15 -0600

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -5,7 +5,7 @@ const app = require('./app');
 const { loadPlanetsData } = require('./models/planets.model')
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URL = "mongodb+srv://joetags:dRagon14@cluster0.k9a3g.mongodb.net/NASA-project?retryWrites=true&w=majority"
+const MONGO_URL = "mongodb+srv://joetags:********@cluster0.k9a3g.mongodb.net/NASA-project?retryWrites=true&w=majority"
 
 const server = http.createServer(app);
 
